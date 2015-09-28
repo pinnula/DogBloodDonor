916fc27f25e7f818ad85e45b871d4f74088430f4
Chakree Kaewchai <chakree30584@gmail.com>
2015-09-28 22:50:17 +0700

    repair dog_add, profile_edit after remove jquery mobile

--- platforms/android/assets/www/assets/js/buttonhandler.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
-        document.addEventListener("backbutton", function (e) {
-            e.preventDefault();
-        }, false );
-}
