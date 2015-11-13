12cf6ad74f658f71eab441fc58f680848ccef798
pinnula <wingpin_pinblue@hotmail.com>
2015-11-13 14:40:24 +0700

    edit link to TestBed

--- www/assets/js/base.js
+++ www/assets/js/base.js
@@ -1,5 +1,5 @@
 //Configuration
-var api = "https://dogblooddonor.in.th/api/"; //api endpoint link
-var userimage = "https://dogblooddonor.in.th/api/userimage/"; //user image link
-var dogimage = "https://dogblooddonor.in.th/api/dogimage/"; //dog image link
+var api = "https://testbed.dogblooddonor.in.th/api/"; //api endpoint link
+var userimage = "https://testbed.dogblooddonor.in.th/api/userimage/"; //user image link
+var dogimage = "https://testbed.dogblooddonor.in.th/api/dogimage/"; //dog image link
 var deviceid = "";
